6ea7fa278abea05d5969da84e987c8531df30752
RymHaouachi <rym.haouachi.ing@gmail.com>
2021-01-02 12:14:52 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Profile from './profile/Profile';
+import orange from './orange.jpg'
 
 function App() {
+  let fullName ='Rym Haouachi';
+  const handleName =()=>{
+    alert ("Hello "+fullName);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+ 
+    
+    <Profile handleName={handleName} fullName = {fullName} profession='Developer'>
+      <img src ={orange} alt=''/>
+    </Profile>
+    
+    
+    </>
   );
 }
 
